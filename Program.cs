97521b1b99b7cbe5316f57fab4035577407fd32e
Boťa Jan <botajan@email.cz>
2021-03-28 20:06:07 +0200

    Základní rozvržení

--- Program.cs
+++ Program.cs
@@ -10,6 +10,45 @@ namespace Cykly_rekurze
     {
         static void Main(string[] args)
         {
+            ConsoleKeyInfo cki;
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Zadej první číslo 'x': ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Zadej druhé číslo 'n': ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("--------------------");
+                Console.WriteLine("Součet pomocí cyklu 'for':        " + SoucetFor(x, n));
+                Console.WriteLine("Součet pomocí cyklu 'while':      " + SoucetWhile(x, n));
+                Console.WriteLine("Součet pomocí cyklu 'do-while':   " + SoucetDoWhile(x, n));
+                Console.WriteLine("Součet pomocí rekurentní metody:  " + SoucetRekurent(x, n));
+                Console.WriteLine("--------------------");
+                Console.WriteLine("Pro ukončení stiskněte 'k'. Jinak jedeme znovu ;)");
+                cki = Console.ReadKey();
+                
+            } while (cki.Key != ConsoleKey.K);
+        }
+
+        private static string SoucetFor(int x, int n)
+        {
+            return "Hi";
+        }
+
+        private static string SoucetWhile(int x, int n)
+        {
+            return "Hi";
+        }
+
+        private static string SoucetDoWhile(int x, int n)
+        {
+            return "Hi";
+        }
+
+        private static string SoucetRekurent(int x, int n)
+        {
+            return "Hi";
         }
     }
 }
