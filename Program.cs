30cc33181fc0dd75fc981657680b3d62e3a8bd25
Boťa Jan <botajan@email.cz>
2021-03-28 19:53:02 +0200

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cykly_rekurze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
